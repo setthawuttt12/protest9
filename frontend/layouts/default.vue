3d1a69f2d96addcb0e477d03b18c1c2e289c3b74
setthawuttt12 <setthawuttt12@gmail.com>
2026-08-15 09:15:31 +0700

    first_commit

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app>
+        <v-app-bar></v-app-bar>
+        <v-main>
+            <slot/>
+        </v-main>
+        <v-footer></v-footer>
+    </v-app>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Hello Nuxt</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
